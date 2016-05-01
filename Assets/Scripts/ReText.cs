bbe32e24c4d651ad9e001e2b35c20b6e667624c5
Lolicon77 <1504296509@qq.com>
2016-05-01 11:22:32 +0800

    ~~

---
+++ Assets/Scripts/ReText.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+using Utils;
+
+public class ReText : MonoBehaviour {
+
+	public TextAsset textAsset;
+
+	public string path;
+	public char[] punctuations = { '.', ';', '!', '?', '。', '！', '？', '”', '；' };
+	public char[] UpperNum = { '一', '二', '三', '四', '五', '六', '七', '八', '九', '十' };
+
+	public string outpath;
+
+	// Use this for initialization
+	void Start() {
+		HandleByFile(path);
+	}
+
+	void Handle(string text) {
+	}
+
+	void HandleByFile(string path) {
+		StringBuilder sb = new StringBuilder();
+		FileStream fs = new FileStream(path, FileMode.Open);
+		if (fs != null) {
+			StreamReader sr = new StreamReader(fs, Encoding.GetEncoding("gb2312"));
+			string line;
+			bool lastLineEndWithPunctuation = false;
+			while ((line = sr.ReadLine()) != null) {
+				line = stringUtils.ToDBC(line);
+				if (line.Length == 0) {
+					continue;
+				}
+				if (line.Contains(' ')) {
+					var count = line.Count(character => character.Equals(' '));
+					if (line.Length == 0 || count > 0.6 * line.Length) {
+						continue;
+					}
+				}
+				if (line.Length < 6 && line.Contains("--")) {
+					continue;
+				}
+				if (line.Trim().Length < 10 && line.Contains("论 神")) {
+					continue;
+				}
+				if (line.Contains('(')) {
+					List<int> temp = new List<int>();
+					for (int i = 0; i < line.Length; i++) {
+						if (line[i] == ('(') && UpperNum.Contains(line[i + 1])) {
+							temp.Add(i);
+						}
+					}
+					for (int i = 0; i < temp.Count; i++) {
+						line = line.Insert(temp[i] + i, "\r\n");
+					}
+				}
+				if (lastLineEndWithPunctuation) {
+					sb.AppendLine(line);
+				} else {
+					sb.Append(line.TrimStart());
+				}
+				lastLineEndWithPunctuation = punctuations.Contains(line[line.Length - 1]);
+			}
+			sr.Close();
+		}
+		fs.Close();
+		FileStream newfs = new FileStream(outpath, FileMode.OpenOrCreate);
+		StreamWriter sw = new StreamWriter(newfs);
+		sw.Write(sb);
+		sw.Close();
+		newfs.Close();
+		Debug.Log("finish");
+	}
+
+	void LineHandle(string text) {
+
+	}
+
+}
---
+++ Assets/Scripts/StringUtils.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+namespace Utils {
+	public class stringUtils : MonoBehaviour {
+		/// 转全角的函数(SBC case)
+		///
+		///任意字符串
+		///全角字符串
+		///
+		///全角空格为12288，半角空格为32
+		///其他字符半角(33-126)与全角(65281-65374)的对应关系是：均相差65248
+		///
+		public static string ToSBC(string input) {
+			// 半角转全角：
+			char[] c = input.ToCharArray();
+			for (int i = 0; i < c.Length; i++) {
+				if (c[i] == 32) {
+					c[i] = (char)12288;
+					continue;
+				}
+				if (c[i] < 127)
+					c[i] = (char)(c[i] + 65248);
+			}
+			return new string(c);
+		}
+
+		/**/
+		// /
+		// / 转半角的函数(DBC case)
+		// /
+		// /任意字符串
+		// /半角字符串
+		// /
+		// /全角空格为12288，半角空格为32
+		// /其他字符半角(33-126)与全角(65281-65374)的对应关系是：均相差65248
+		// /
+		public static string ToDBC(string input) {
+			char[] c = input.ToCharArray();
+			for (int i = 0; i < c.Length; i++) {
+				if (c[i] == 12288) {
+					c[i] = (char)32;
+					continue;
+				}
+				if (c[i] > 65280 && c[i] < 65375)
+					c[i] = (char)(c[i] - 65248);
+			}
+			return new string(c);
+		}
+
+
+		/// <summary>
+		/// GB2312转换成UTF8
+		/// </summary>
+		/// <param name="text"></param>
+		/// <returns></returns>
+		public static string gb2312_utf8(string text) {
+			//声明字符集   
+			System.Text.Encoding utf8, gb2312;
+			//gb2312   
+			gb2312 = System.Text.Encoding.GetEncoding("gb2312");
+			//utf8   
+			utf8 = System.Text.Encoding.GetEncoding("utf-8");
+			byte[] gb;
+			gb = gb2312.GetBytes(text);
+			gb = System.Text.Encoding.Convert(gb2312, utf8, gb);
+			//返回转换后的字符   
+			return utf8.GetString(gb);
+		}
+
+		/// <summary>
+		/// UTF8转换成GB2312
+		/// </summary>
+		/// <param name="text"></param>
+		/// <returns></returns>
+		public static string utf8_gb2312(string text) {
+			//声明字符集   
+			System.Text.Encoding utf8, gb2312;
+			//utf8   
+			utf8 = System.Text.Encoding.GetEncoding("utf-8");
+			//gb2312   
+			gb2312 = System.Text.Encoding.GetEncoding("gb2312");
+			byte[] utf;
+			utf = utf8.GetBytes(text);
+			utf = System.Text.Encoding.Convert(utf8, gb2312, utf);
+			//返回转换后的字符   
+			return gb2312.GetString(utf);
+		}
+	}
+}
